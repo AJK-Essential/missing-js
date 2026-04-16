2ed082b91f5cc474c682143ab14a6a26ddcd40d1
AJK-Essential <essentialcoder@outlook.com>
2026-04-16 21:23:13 +0530

    experimental swipe logic on requestanimationframe

--- packages/page-virtualizer/src/page-virtualizer-two.ts
+++ packages/page-virtualizer/src/page-virtualizer-two.ts
@@ -796,11 +796,12 @@ export class MissingPageVirtualizerTwo extends virtualiserKeyboardBase {
       e.preventDefault();
       return;
     }
-
-    const swipeEvent = e as MissingSwipePhysicsEvent;
-    const scrollDelta = -swipeEvent.detail.deltaY * this.swipeDeltaMultiplier;
-    this.slowScrollBy(scrollDelta, true);
-    this.dispatchEvent(new CustomEvent("scrolling"));
+    requestAnimationFrame(() => {
+      const swipeEvent = e as MissingSwipePhysicsEvent;
+      const scrollDelta = -swipeEvent.detail.deltaY * this.swipeDeltaMultiplier;
+      this.slowScrollBy(scrollDelta, true);
+      this.dispatchEvent(new CustomEvent("scrolling"));
+    });
   }
   /**
    * This function is for highly accurate jumps to a particular
